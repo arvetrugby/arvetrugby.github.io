85a86a9f815bfe2fc245c0da9c137d7f8e121fa8
arvetrugby <arvetrugby@gmail.com>
2026-03-14 20:41:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2058,7 +2058,7 @@ async function cargarQuienesSomosExistente() {
     // Guardar Quiénes Somos
     if (btnGuardarQuienesSomos) {
         btnGuardarQuienesSomos.addEventListener('click', async function() {
-            mostrarLoader('Guardando información...');
+           
             showMsg('Guardando información...', 'info');
             
             try {
@@ -2072,7 +2072,7 @@ async function cargarQuienesSomosExistente() {
                         fechaFundacion: fechaFundacionInput ? fechaFundacionInput.value : '',
                         colores: coloresInput ? coloresInput.value : ''
                     })
-                    ocultarLoader();
+                    
                 });
                 
                 const result = await response.json();

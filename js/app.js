05cd05b7dc5b5c0bc2e343f95dc63b31e7da5080
arvetrugby <arvetrugby@gmail.com>
2026-03-14 22:13:10 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1529,7 +1529,7 @@ window.cambiarEstadoJugador = async function(id, nuevoEstado) {
     });
 
     if (response.success) {
-        window.showMsg('Estado actualizado', 'success');
+        showMsg('Actualizado...', 'info');
         cargarJugadoresAdmin();
     } else {
         alert('Error: ' + response.error);
@@ -1544,7 +1544,7 @@ window.eliminarJugador = async function(id) {
     });
 
     if (response.success) {
-        window.showMsg('Eliminado', 'success');
+        showMsg('Eliminado...', 'info');
         cargarJugadoresAdmin();
     } else {
         alert('Error: ' + response.error);
